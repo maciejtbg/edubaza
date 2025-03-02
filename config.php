a40f682fc2e4915245c4628c31cba947229d18ff
Maciej <99690787+maciejtbg@users.noreply.github.com>
2025-03-02 19:59:07 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Konfiguracja połączenia z bazą danych – zmodyfikuj dane według swoich potrzeb
-define('DB_HOST', 'sql307.infinityfree.com');
-define('DB_NAME', 'if0_38418645_edubaza');
-define('DB_USER', 'if0_38418645');
-define('DB_PASS', '5Kilosowow');
-?>
